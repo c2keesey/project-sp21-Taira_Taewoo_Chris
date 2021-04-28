f1eaa74794185a07c65fc2d23cc2039acf370fc5
Taira Fujioka <taira.f20@gmail.com>
2021-04-28 12:53:03 -0700

    update

--- solver.py
+++ solver.py
@@ -4,6 +4,7 @@ from utils import is_valid_solution, calculate_score
 import sys
 from os.path import basename, normpath
 import glob
+import math
 
 
 def solve(G):
@@ -24,6 +25,53 @@ def solve(G):
     else:
         k_max = 100
         c_max = 5
+
+    c = []
+    k = []
+    curr_G = G.copy()
+    curr = nx.shortest_path_length(G, 0, v_size-1, 'weight')
+    while k_max > 0 or c_max > 0:
+        max_len = -float('inf')
+        e_choice = None
+        v_choice = None
+        path = nx.shortest_path(G, 0, v_size-1, 'weight')
+
+        # Checking every removal of a vertex
+        if k_max > 0:
+            for v in path:
+                if v == 0 or v == v_size-1:
+                    continue
+                H = G.copy()
+                H.remove_node(v)
+                shortest_path_length = nx.shortest_path_length(H, 0, v_size-1, 'weight')
+                if max_len < shortest_path_length:
+                    v_choice = v
+                    max_len = shortest_path_length
+
+        # Checking every removal of an edge
+        edge = False
+        if c_max > 0:
+            for idx in range(0, len(path)-1):
+                e = (path[idx], path[idx+1])
+                H = G.copy()
+                H.remove_edge(e)
+                shortest_path_length = nx.shortest_path_length(H, 0, v_size - 1, 'weight')
+                if max_len < shortest_path_length:
+                    e_choice = e
+                    max_len = shortest_path_length
+                    edge = True
+
+        if not curr < max_len:
+            break
+        else:
+            curr = max_len
+            if edge:
+                curr_G = curr_G.remove_edge(e_choice)
+                k_max -= 1
+            else:
+                curr_G = curr_G.remove_vertex(v_choice)
+                c_max -= 1
+
 
 
 
